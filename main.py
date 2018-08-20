7e84124ebba68d531fc1e6ecd796ad12fe9dcdfa
Dahu Qi <dahu@moven.com>
2018-08-20 16:05:03 -0400

    kick off

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 ## the main project entrance
+
+import cv2 
+import time 
+import queue as Q 
+import thread 
+import time 
+
+
+# Section 1. Image acquisition from laptop camera (id=0), typical I/O, typical thread issue
+
+
+
+
+
+# Section 2. Object recognition, CPU intensitive.
+
+
+
+# Section 3. Image edit and display. CPU intensitive.
