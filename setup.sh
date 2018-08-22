49be4533a2b4b9d65f646ceacd6cef09e1201cf7
Dahu Qi <dahu@moven.com>
2018-08-22 10:23:52 -0400

    environment setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-# This is the setup for mac, not ubuntu
+# This is the setup for ubuntu
+
+apt-get update && apt-get update && apt-get dist-upgrade -y
+# opencv dependecy libs
+apt-get install -y libsm6 libxext6 libxrender-dev
+apt-get install -y python python-pip
+pip install opencv-python
