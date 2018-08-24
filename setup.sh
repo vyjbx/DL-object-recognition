089093a5a8dfbd641965a674bc38c7a0008a4db1
Dahu Qi <dahu@moven.com>
2018-08-24 15:59:05 -0400

    downloading curl, model

--- setup.sh
+++ setup.sh
@@ -2,8 +2,16 @@
 
 # This is the setup for ubuntu
 
-apt-get update && apt-get update && apt-get dist-upgrade -y
+apt-get update && apt-get upgrade && apt-get dist-upgrade -y
+apt-get install -y curl 
 # opencv dependecy libs
 apt-get install -y libsm6 libxext6 libxrender-dev
 apt-get install -y python python-pip
 pip install opencv-python
+
+
+
+# download coco_v1
+mkdir model
+cd model
+http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v1_coco_11_06_2017.tar.gz
